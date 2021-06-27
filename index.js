b29fb1a01a951d31f27c1e960c29f7d074ec5537
Old Prego <spymasterluke@gmail.com>
2021-06-27 15:48:59 -0500

    Finished all the routing to do with employees

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const inquirer = require('inquirer');
 const db = require('./db');
+const { prompt } = require('inquirer');
+require("console.table");
 
 mainMenu();
 
@@ -24,6 +26,10 @@ async function mainMenu() {
                         value: "newDep"
                     },
                     {
+                        name: "Add New Role",
+                        value: "newRol"
+                    },
+                    {
                         name: "View Employees",
                         value: "viewEmp"
                     },
@@ -36,9 +42,14 @@ async function mainMenu() {
                         value: "viewDepts"
                     },
                     {
+                        name: "View Roles",
+                        value: "viewRol"
+                    },
+                    {
                         name: "Update Employee Roles",
-                        value: "upRole"
-                    }
+                        value: "updEmpRol"
+                    },
+                    
                 ]
             }
         ]);
@@ -46,12 +57,24 @@ async function mainMenu() {
         switch(operation){
             case "newEmp":
                 newEmployee();
+            case "newMgr":
+                newManager();
+            case "newDep":
+                newDept();
+            case "newRol":
+                newRole();
             case "viewEmp":
-                console.log(db.viewEmployees());
+                viewEmployees();
             case "viewDepts":
                 viewDepartments();
             case "viewMgr":
                 viewManagers();
+            case "viewRol":
+                viewRoles();
+            case "updEmpRol":
+                updEmpRol();
+            default:
+                return exitMenu();
         }
 }
 
@@ -69,4 +92,61 @@ async function newEmployee() {
                 message: "What is the employee's last name?"
             }
         ])
+    }
+
+async function viewEmployees() {
+    const employees = await db.findAllEmployees();
+
+    console.log("\n");
+    console.table(employees);
+
+    mainMenu();
+}
+
+async function updEmpRol(){
+    const employees = await db.findAllEmployees();
+
+    const empList = employees.map(({ id, first_name, last_name }) => ({
+        name: `${first_name} ${last_name}`,
+        value: id
+    }));
+
+    const { empID } = await prompt([
+        {
+            type: "list",
+            name: "empID",
+            message: "Which employee's role do you want to update?",
+            choices: empList
+        }
+    ]);
+
+    const roles = await db.findAllRoles();
+
+    const roleChoices = roles.map(({ id, title}) => ({
+        name: title,
+        value: id
+    }));
+
+    const { roleID } = await prompt([
+        {
+            type: "list",
+            name: "roleID",
+            message: "Which role would you like to set?",
+            choices: roleChoices
+        }
+    ]);
+
+    await db.updateEmployeeRole(empID, roleID);
+
+    console.log("The Employee's role has been updated.");
+
+    mainMenu();
+}
+
+async function newDept(){
+    
+}
+
+function exitMenu() {
+    process.exit();
 }
